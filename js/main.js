63a180ca5d4fbb58d6a3cfc7520a2c0b60a445bc
ornette coleman <omniosi@gmail.com>
2014-05-23 20:16:09 -0400

    front page mobile start.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 (function(){
 
-	var cart = document.getElementById('cart');
+var cart = document.getElementById('cart'),
+				moreless = document.getElementsByClassName('moreless')[0],
+				cont = document.getElementsByClassName('cont')[0];
 	
 window.addEventListener('resize',function(event){
 	var docwidth = window.innerWidth;
@@ -12,6 +14,18 @@ window.addEventListener('resize',function(event){
 	}	
 	console.log("addEventListener called");
 	console.log("docwidth = "+docwidth);
+});
+
+cont.style.display = 'none';
+moreless.addEventListener('click',function(){
+	// if(cont.style.display = 'none'){
+	if(moreless.innerHTML === 'more'){
+		cont.style.display = 'block';
+		moreless.innerHTML = 'less';
+	}else{
+		cont.style.display = 'none';
+		moreless.innerHTML = 'more';
+	}
 });
 
 })();
