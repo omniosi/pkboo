5476d6ca46e0d46abddaa0ab8fd7f37c1825aa92
ornette coleman <omniosi@gmail.com>
2015-11-09 14:54:05 -0800

    simple layout for easy store start

--- js/main.js
+++ js/main.js
@@ -3,14 +3,13 @@
 var cart = document.getElementById('cart'),
 				moreless = document.getElementsByClassName('moreless')[0],
 				cont = document.getElementsByClassName('cont')[0];
-	
+
 cont.style.display = 'none';
 moreless.addEventListener('click',openclose);
 
 window.addEventListener('resize',function(event){
 	var docwidth = window.innerWidth;
 
-	// if(window.innerWidth>700){
 	if(window.innerWidth>749){
 		cart.style.top = '0px';
 
@@ -18,14 +17,14 @@ window.addEventListener('resize',function(event){
 		moreless.style.display = 'none';
 
 	}	else {
-		cart.style.top = '25px';			
+		cart.style.top = '25px';
 
 		cont.style.display = 'none';
 		moreless.style.display = 'block';
 
-	}	
-	console.log("addEventListener called");
-	console.log("docwidth = "+docwidth);
+	}
+	// console.log("addEventListener called");
+	// console.log("docwidth = "+docwidth);
 });
 
 function openclose(){
